c037663ac29673d77319a1c90bf336c1dd8d3f29
OlaStalberg <olastaalberg@gmail.com>
2019-08-01 13:37:06 +0200

    Recursive works but UI not update on button click

--- src/index.js
+++ src/index.js
@@ -1,79 +1,70 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-function NavigationApp(props) {
+function NavigationApp() {
+    const [url, setURL] = useState('https://play-with-fint.felleskomponent.no/utdanning/elev/person/fodselsnummer/14029923273');
+    const [json, setJson] = useState('');
 
-    function getInformation(jsonString) {
-        let extractedInformation = jsonString;
-        let findLinksStartIndex = extractedInformation.indexOf("_links");
-        extractedInformation = extractedInformation.substring(0, findLinksStartIndex - 2);
-        return extractedInformation.concat("}");
-    }
+    useEffect(()=> {
+        fetch(url)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setJson(result);
+                }
+            )
+    });
 
-    const informationData = getInformation(props.data);
-    const linksData = getLinks(props.data);
-
-    return (
-        <div>
-            <Header/>
-            <SideBar/>
-            <InformationField data={informationData}></InformationField>
-            <LinksField data={linksData}></LinksField>
-        </div>
-
-    );
-}
-
-const LinksField = ({data}) => {
-    if (!data) {
-        return null;
-    }
-    if (typeof data === 'string' || typeof data === 'number' || typeof data === 'boolean'){
-        return <p>{data}</p>
-    }
-    let propertyKeys = [];
-    if (Array.isArray(data)) {
-        for (let entry in data) {
-            propertyKeys.push(entry);
-        }
-        if (propertyKeys.length === 0) {
-            return <p>{data}</p>
-        }
-        return (<div>{propertyKeys.map((entry) => {
-                return <div>
-                    <LinksField data={data[entry]}/>
-                </div>
+    const DataField = ({data}) => {
+        let propertyKeys = [];
+        if (Array.isArray(data)) {
+            for (let entry in data) {
+                propertyKeys.push(entry);
             }
-        )}
-        </div>);
-        //return <p>{data[0].href}</p>;
-    } else {
-        propertyKeys = Object.keys(data);
             if (propertyKeys.length === 0) {
                 return <p>{data}</p>
             }
-        return (<div>{propertyKeys.map((key) => {
-                return <div>
-                    <p>{key}</p>
-                    <LinksField data={data[key]}/>
-                </div>
+            return (<div>{propertyKeys.map((entry) => {
+                    return <div>
+                        <DataField data={data[entry]}/>
+                    </div>
+                }
+            )}
+            </div>);
+        } else if (typeof data === 'object' && data !== null) {
+            propertyKeys = Object.keys(data);
+            if (propertyKeys.length === 0) {
+                return <p>{data}</p>
             }
-        )}
-        </div>);
-    }
+            return (<div>{propertyKeys.map((key) => {
+                    return <div>
+                        <p>{key}</p>
+                        <DataField data={data[key]}/>
+                    </div>
+                }
+            )}
+            </div>);
+        } else {
+            if (typeof data === 'string' && validURL(data)){
+                return <button onClick={()=> setURL(data)}>TEST</button>
+            }
+            return <p><b>{data}</b></p>;
+        }
+    };
+
+        return (
+            <div>
+                <Header/>
+                <DataField data={json}></DataField>
+            </div>
+        );
 }
 
-function InformationField(props) {
-    return (
-        <div className={InformationField}>
-            <p>{props.data}</p>
-        </div>
-    )
-}
 
-function Header(props) {
+
+function Header() {
     return (
         <div className={Header}>
             <h1>Velkommen til play-with-fint navigasjon</h1>
@@ -81,24 +72,19 @@ function Header(props) {
     );
 }
 
-function SideBar(props) {
-    return (
-        <div className={SideBar}>
-            <h1>Her kommer det en "side bar"</h1>
-        </div>
-    );
-}
-
-function getLinks(jsonString) {
-    return JSON.parse(jsonString)._links;
-
-}
-
-const dataFromServer = '{"kontaktinformasjon":{"epostadresse":"RoseSta@jourrapide.com","mobiltelefonnummer":"48213268"},"postadresse":{"adresselinje":["Setra vei 207"],"postnummer":"0786","poststed":"Oslo"},"bostedsadresse":{"adresselinje":["Setra vei 207"],"postnummer":"0786","poststed":"Oslo"},"fodselsdato":"1999-02-14T00:00:00Z","fodselsnummer":{"identifikatorverdi":"14029923273"},"navn":{"etternavn":"Støa","fornavn":"Rose"},"_links":{"elev":[{"href":"https://play-with-fint.felleskomponent.no/utdanning/elev/elev/elevnummer/500001"}],"self":[{"href":"https://play-with-fint.felleskomponent.no/utdanning/elev/person/fodselsnummer/14029923273"}]}}';
-
-ReactDOM.render(<LinksField data={getLinks(dataFromServer)}/>, document.getElementById('root'));
+ReactDOM.render(<NavigationApp/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+function validURL(str) {
+    const pattern = new RegExp('^(https?:\\/\\/)?'+
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+
+        '((\\d{1,3}\\.){3}\\d{1,3}))'+ //
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+
+        '(\\?[;&a-z\\d%_.~+=-]*)?'+
+        '(\\#[-a-z\\d_]*)?$','i');
+    return !!pattern.test(str);
+}
